38b5cc3a32ddb3c74da244594b1885116f5b5d03
Sebastiao Vinicius Araujo de Moura <sebastianbavini@hotmail.com>
2024-04-26 18:48:21 -0300

    Pixels Escalonaveis e tamanhos e alturas de textos

---
+++ app/src/main/java/com/sebastiaomoura/happybirthday/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.sebastiaomoura.happybirthday
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.sebastiaomoura.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+    Text(text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp)
+    Text(
+        text = from,
+        fontSize = 36.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Feliz Aniversário, Deus!", from= "From Gautama")
+    }
+}
